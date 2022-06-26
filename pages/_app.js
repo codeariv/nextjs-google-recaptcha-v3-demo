63710aaae8f479bb9f0862e1638c8151281f9a50
Techomoro <techomoroblog@gmail.com>
2022-06-26 18:48:03 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey="your_site_key"
+      scriptProps={{
+        async: false,
+        defer: false,
+        appendTo: "head",
+        nonce: undefined,
+      }}
+    >
+      <Component {...pageProps} />
+    </GoogleReCaptchaProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/api/enquiry.js
@@ -0,0 +1,44 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const handler = (req, res) => {
+  if (req.method === "POST") {
+    try {
+      fetch("https://www.google.com/recaptcha/api/siteverify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `secret=your_secret_key&response=${req.body.gRecaptchaToken}`,
+      })
+        .then((reCaptchaRes) => reCaptchaRes.json())
+        .then((reCaptchaRes) => {
+          console.log(
+            reCaptchaRes,
+            "Response from Google reCatpcha verification API"
+          );
+          if (reCaptchaRes?.score > 0.5) {
+            // Save data to the database from here
+            res.status(200).json({
+              status: "success",
+              message: "Enquiry submitted successfully",
+            });
+          } else {
+            res.status(200).json({
+              status: "failure",
+              message: "Google ReCaptcha Failure",
+            });
+          }
+        });
+    } catch (err) {
+      res.status(405).json({
+        status: "failure",
+        message: "Error submitting the enquiry form",
+      });
+    }
+  } else {
+    res.status(405);
+    res.end();
+  }
+};
+
+export default handler;
